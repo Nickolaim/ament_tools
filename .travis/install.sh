24138dd294d8a9e44e0dc548ca92aaf054fb66eb
Nick Medveditskov <Nickolaim@users.noreply.github.com>
2018-03-26 13:10:31 -0700

    Travis CI integration on Mac + Code coverage (#2)

---
+++ .travis/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -x
+
+do_install()
+{
+    set -e
+
+    if [[ $TRAVIS_OS_NAME == 'osx' && $PYTHON_INSTALLER == 'pyenv' ]]; then
+        brew install pyenv-virtualenv
+        pyenv versions
+        eval "$(pyenv init -)"
+        pyenv install $TRAVIS_PYTHON_VERSION
+        PYTHON_ENV_NAME=virtual-env-$TRAVIS_PYTHON_VERSION
+        pyenv virtualenv $TRAVIS_PYTHON_VERSION $PYTHON_ENV_NAME
+        pyenv activate $PYTHON_ENV_NAME
+
+    elif [[ $TRAVIS_OS_NAME == 'osx' && $PYTHON_INSTALLER == 'brew' ]]; then
+        brew list
+        brew upgrade python3
+        # nose 1.3.7 creates /usr/local/man dir if it does not exist.
+        # The operation fails because current user does not own /usr/local.  Create the dir manually instead.
+        # More at https://github.com/nose-devs/nose/issues/1017
+        sudo mkdir /usr/local/man
+        sudo chown -R $(whoami) $(brew --prefix)/*
+
+        export PATH=$(pwd)/.travis/shim:$PATH
+    fi
+
+    set +e
+}
+
+do_install
